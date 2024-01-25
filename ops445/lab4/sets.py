97f8b71780c07a7338d8b35baf7596bff4d833f0
Sithum Kavmal Bamunuarachchi <sbamunuarachchi@myseneca.ca>
2024-01-25 02:47:56 -0500

    Added lab4c

--- ops445/lab4/sets.py
+++ ops445/lab4/sets.py
@@ -8,8 +8,8 @@ print('Ix' in s1)
 print(s1)
 print(s2)
 
-print(s2 | s3)
-print(s2.union(s3))
+print(s2 | s3)         # returns a set containing all values from both sets
+print(s2.union(s3))    # same as s2 | s3
 
 print(s2 & s3)             # returns a set containing all values that s2 and s3 share
 print(s2.intersection(s3)) # same as s2 & s3
